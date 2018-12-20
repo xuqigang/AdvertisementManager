55554cff6b05fab9515f1985838d2e1cd93cb6a4
xuqigang <xuqigang10@163.com>
2018-12-20 19:42:06 +0800

    创建项目提交到master

---
+++ AdvertisementManager/AdvertisementManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdvertisementManager
+//
+//  Created by 韩肖杰 on 2018/12/20.
+//  Copyright © 2018 xuqigang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AdvertisementManager/AdvertisementManager/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AdvertisementManager
+//
+//  Created by 韩肖杰 on 2018/12/20.
+//  Copyright © 2018 xuqigang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
